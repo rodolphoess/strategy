f6364937780e4df918eb45ba0964b6d3b22e4cc3
Rodolpho Erick <rodolphoerick90@gmail.com>
2018-11-13 12:04:03 -0300

    Comentários sobre o uso do strategy

--- src/strategy/FelinoCliente.java
+++ src/strategy/FelinoCliente.java
@@ -4,6 +4,12 @@ package strategy;
  * @author Rodolpho E.
  * 
  * Implementação baseada no artigo https://www.thiengo.com.br/padrao-de-projeto-strategy-estrategia#title-2
+ * 
+ * Usa-se o padrão de projeto strategy nas seguintes situações:
+ * 1) Quando uma classe possui muitos comportamentos que aparecem com diversos IFs
+ * 2) Quando muitas classes relacionadas diferetem apenas no seu comportamento
+ * 3) Quando necessita-se de variações do algoritmo de classes
+ * 4) Quando necessita-se ocultar estruturas de dados complexas 
  *
  */
 public class FelinoCliente {
